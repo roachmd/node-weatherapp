94a69475f037ad1b6af2a767a27e0ed9d03bdbea
Michael D Roach <roach.d.michael@gmail.com>
2018-07-03 00:26:59 -0400

    On branch master  Your branch is up to date with 'origin/master'.
    	new file:   playground/promise.js
    	new file:   playground/promise2.js

---
+++ playground/promise.js
@@ -0,0 +1,36 @@
+var asyncAdd = (a,b) => {
+    return new Promise((resolve, reject) =>{
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b == 'number') {
+                resolve(a + b);
+            } else {
+                reject("Arguments must be numbers");
+            }
+
+        }, 1500);
+    });
+};
+
+asyncAdd(5,'7').then((res) => {
+    console.log('Result: ', res );
+    return asyncAdd(res,33);
+}, (errorMessage) => {
+    console.log(errorMessage);
+}).then((res) => {
+    console.log('Should be 45', res);
+}).catch((errorMessage) => {
+    console.log(errorMessage);
+});
+
+// var somePromise = new Promise( (resolve, reject) => {
+//     setTimeout(() => {
+//          reject ("unable to fulfill promise");
+//         // resolve("hey it worked!");
+//     }, 2500);
+// });
+//
+// somePromise.then((message) => {
+//     console.log('Success', message);
+// }, (errorMessage) => {
+//     console.log('Error: ', errorMessage);
+// })
